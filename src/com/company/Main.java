093bad2fe259b088e80256a674beea8c2e6fcb49
Gabe <gabecurran01@gmail.com>
2022-03-07 13:23:06 -0500

    Setup Selenium base with git

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,21 @@
 package com.company;
 
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.JavascriptExecutor;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        WebDriver driver;
+        System.setProperty("webdriver.chrome.driver", "../resources/chromedriver");
+        driver = new ChromeDriver();
+        driver.get("https://www.google.com");
     }
+
 }
